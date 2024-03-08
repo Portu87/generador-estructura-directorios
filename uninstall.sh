b6e8a66a1a7e13a32c5c23e4c531e3739a1439ad
Portu87 <sebas.p.g87@gmail.com>
2024-03-08 18:41:28 +0100

    fix uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -19,7 +19,5 @@ sudo chmod +w /usr/local/bin/generar_arbol.py
 # Elimina el archivo desde /usr/local/bin
 sudo rm /usr/local/bin/generar_arbol.py
 
-# Elimina todos los árboles creados en la carpeta arboles
-rm -r arboles
 
 echo "La desinstalación se ha completado correctamente."
